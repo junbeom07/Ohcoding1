71bf612439d276ad0d03c63b4146d10a5451cec9
junbeom07 <133008219+junbeom07@users.noreply.github.com>
2023-12-01 15:52:25 +0900

    Delete Ohcoding1-main/Ohcoding1-main directory

--- Ohcoding1-main/Ohcoding1-main/새 폴더/main.js
+++
@@ -1,9 +0,0 @@
-const randomDiv = document.querySelector("div");
-const button = document.querySelector("button");
-
-function getRandomNum() {
-  const randomNum = Math.random();
-  randomDiv.innerText = randomNum;
-}
-
-button.addEventListener("click", getRandomNum);
